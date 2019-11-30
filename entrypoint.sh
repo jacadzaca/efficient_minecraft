1f5636cb1f6a36e5832204106147c0c42be3d21e
jacadzaca <vitouejj@gmail.com>
2019-11-30 22:49:53 +0100

    added flags, so the jvm works better

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar minecraft_server.1.12.jar
+java -Xgc:scvNoAdaptiveTenure -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:concurrentScavenge -Xdisableexplicitgc -Xtune:virtualized -jar minecraft_server.1.12.jar
