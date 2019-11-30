0ecd9dc9cfd57b8995c88b7e118f45a456d1fa2f
jacadzaca <vitouejj@gmail.com>
2019-11-30 23:38:16 +0100

    container should now use less memory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 HEAP_SIZE=2096
 
-NURSERY_MINIMUM=$(($HEAP_SIZE / 2))
 NURSERY_MAXIMUM=$(($HEAP_SIZE * 4 / 5))
 
-java -Xgc:scvNoAdaptiveTenure -Xms${HEAP_SIZE}M -Xmx${HEAP_SIZE}M -Xmns${NURSERY_MINIMUM}M -Xmnx${NURSERY_MAXIMUM}M -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:concurrentScavenge -Xdisableexplicitgc -Xtune:virtualized -jar $MINECRAFT_JAR
+java -Xgc:scvNoAdaptiveTenure -Xmx${HEAP_SIZE}M -Xmnx${NURSERY_MAXIMUM}M -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:concurrentScavenge -Xdisableexplicitgc -Xtune:virtualized -jar $MINECRAFT_JAR
