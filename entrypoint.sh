f0e3e6c1afd5ab63cdcf3c4ea24afe04f2e29aa8
jacadzaca <vitouejj@gmail.com>
2019-11-30 23:14:47 +0100

    added more options

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-java -Xgc:scvNoAdaptiveTenure -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:concurrentScavenge -Xdisableexplicitgc -Xtune:virtualized -jar minecraft_server.1.12.jar
+HEAP_SIZE=2096
+
+NURSERY_MINIMUM=$(($HEAP_SIZE / 2))
+NURSERY_MAXIMUM=$(($HEAP_SIZE * 4 / 5))
+
+java -Xgc:scvNoAdaptiveTenure -Xms${HEAP_SIZE}M -Xmx${HEAP_SIZE}M -Xmns${NURSERY_MINIMUM}M -Xmnx${NURSERY_MAXIMUM}M -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:concurrentScavenge -Xdisableexplicitgc -Xtune:virtualized -jar minecraft_server.1.12.jar
