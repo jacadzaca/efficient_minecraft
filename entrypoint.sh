5068fe5447ae7d953acdbaa080ca09ffe988699c
jacadzaca <vitouejj@gmail.com>
2019-12-23 19:15:00 +0100

    entrypoint script should now be more readable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,10 @@ HEAP_SIZE=2096
 
 NURSERY_MAXIMUM=$(($HEAP_SIZE * 4 / 5))
 
-java -Xgc:scvNoAdaptiveTenure -Xmx${HEAP_SIZE}M -Xmnx${NURSERY_MAXIMUM}M -Xgc:dnssExpectedTimeRatioMaximum=3 -Xgc:concurrentScavenge -Xdisableexplicitgc -Xtune:virtualized -jar server.jar
+java -Xgc:scvNoAdaptiveTenure \
+    -Xmx${HEAP_SIZE}M -Xmnx${NURSERY_MAXIMUM}M \
+    -Xgc:dnssExpectedTimeRatioMaximum=3 \
+    -Xgc:concurrentScavenge \
+    -Xdisableexplicitgc \
+    -Xtune:virtualized \
+    -jar server.jar
