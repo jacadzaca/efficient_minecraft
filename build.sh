f23205e5371b2d85f7bbff6a47d107745169cdd8
jaca <vitouejj@gmail.com>
2020-07-16 19:25:00 +0200

    remove tag argument from build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-while getopts ":v:t:tag:vForge:" opt; do
+while getopts "v:t:vForge:" opt; do
     case $opt in
         v )
             VERSION=$OPTARG
@@ -7,15 +7,12 @@ while getopts ":v:t:tag:vForge:" opt; do
         t )
             TYPE=$OPTARG
             ;;
-        tag )
-            IMAGE_TAG=$OPTARG
-            ;;
         vForge )
             FORGE_VERSION=$OPTARG
             ;;
         * )
-            echo "Usage: build.sh [-v] (version) [-t] (type) [-tag] (docker iamge tag) [-vForge] (forge version)"
-            echo "Example: build.sh -v 1.12.2 -t forge -tag minecraft_server_1.12.2_forge_test"
+            echo "Usage: build.sh [-v] (version) [-t] (type) [-vForge] (forge version)"
+            echo "Example: build.sh -v 1.12.2 -t forge"
             exit 126
             ;;
     esac
@@ -28,7 +25,4 @@ done
 [ -z $VERSION ] \
     && VERSION=$(curl https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.latest | .release')
 
-[ -z $IMAGE_TAG ] \
-    && IMAGE_TAG=minecraft_server:$VERSION-$TYPE
-
-docker build --tag $IMAGE_TAG --build-arg MINECRAFT_VERSION=$VERSION --build-arg TYPE=$TYPE --build-arg FORGE_VERSION=$FORGE_VERSION .
+docker build --tag "minecraft_server:$VERSION-$TYPE" --build-arg MINECRAFT_VERSION=$VERSION --build-arg TYPE=$TYPE --build-arg FORGE_VERSION=$FORGE_VERSION .
