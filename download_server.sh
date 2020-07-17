c44328cce1ff5c868bd634577dbfd8d0a96a8942
jaca <vitouejj@gmail.com>
2020-07-17 19:22:57 +0200

    adhere to linter advices

--- download_server.sh
+++ download_server.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-[ -z $MINECRAFT_VERSION ] \
+[ -z "$MINECRAFT_VERSION" ] \
     && echo "No Minecraft version specifed!" \
     && echo "Usage: build_image.sh [-v] (game version)" \
     && exit 1
@@ -14,22 +14,22 @@ case $TYPE in
         ;;
     forge )
         #this section requires the $FORGE_VERSION variable to be specifed
-        [ -z $FORGE_VERSION ] \
+        [ -z "$FORGE_VERSION" ] \
             && echo "Cannot build an forge image, forge version missing" \
             && echo "Usage: build_image.sh [-f] (forge version)"
         FORGE_URL='http://files.minecraftforge.net/maven/net/minecraftforge/forge/'"$MINECRAFT_VERSION"''-"$FORGE_VERSION"'/forge-'"$MINECRAFT_VERSION"'-'"$FORGE_VERSION"'-installer.jar'
         FORGE_INSTALLER_JAR='forge-'"$MINECRAFT_VERSION"'-'"$FORGE_VERSION"'-installer.jar'
-        curl -o $FORGE_INSTALLER_JAR $FORGE_URL && \
-            java -jar $FORGE_INSTALLER_JAR --installServer && \
-            rm $FORGE_INSTALLER_JAR $FORGE_INSTALLER_JAR.log && \
+        curl -o "$FORGE_INSTALLER_JAR $FORGE_URL" && \
+            java -jar "$FORGE_INSTALLER_JAR" --installServer && \
+            rm "$FORGE_INSTALLER_JAR" "$FORGE_INSTALLER_JAR.log" && \
             mv forge-*.jar server.jar
         ;;
     spigot )
         BUILD_TOOLS_URL="https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
         BUILD_TOOLS_JAR="BuildTools.jar"
         apk update --no-cache && apk add --no-cache git
-        curl $BUILD_TOOLS_URL -o $BUILD_TOOLS_JAR && \
-            java -jar $BUILD_TOOLS_JAR --rev $MINECRAFT_VERSION && \
+        curl "$BUILD_TOOLS_URL" -o "$BUILD_TOOLS_JAR" && \
+            java -jar "$BUILD_TOOLS_JAR" --rev "$MINECRAFT_VERSION" && \
             rm $BUILD_TOOLS_JAR && \
             mv spigot-*.jar server.jar
         apk del git
